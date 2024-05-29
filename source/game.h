d07522bfd571557fe9553226f71c69740b82b0e1
Jordan Cisneros <hotdog9955@gmail.com>
2024-05-29 14:10:33 -0400

    added a .h file for the game loop n stuff

---
+++ source/game.h
@@ -0,0 +1,17 @@
+/** @file game.h
+ * 
+ * @brief A module for the main game loop and graphics and such, TODO
+ */ 
+
+#ifndef GAME_H
+#define GAME_H
+
+
+
+void game_loop(){
+    // retrieve stats from car,
+    // change sprites on screen accordingly
+}
+
+
+#endif
